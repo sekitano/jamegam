3c962bf69246ad08bceb9e351d3b4a264d0ad7a3
sekitano <mauziofhope07@gmail.com>
2026-07-25 05:15:53 +0100

    last fixes on op cutscene

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 @onready var fade_out: AnimationPlayer = $ColorRect/AnimationPlayer
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fade_out.play("fade_out")
+	timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	fade_out.play("fade_out")
