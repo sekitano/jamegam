efbcd611dd7626f0e137a4a0331e8b6a47f1fcd5
sekitano <mauziofhope07@gmail.com>
2026-07-25 16:58:38 +0100

    blanket movement and toggle work, might need to adjust a little

--- scenes/magic_blanket.gd
+++ scenes/magic_blanket.gd
@@ -1,8 +1,11 @@
-extends StaticBody2D
+extends Node2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatableBody2D/AnimatedSprite2D
+@onready var static_body: StaticBody2D = $StaticBody2D
+@onready var anim_body: AnimatableBody2D = $AnimatableBody2D
 
-const SPEED = 30
+
+const SPEED = 10.0
 
 var last_pos: Vector2 
 var follow_player := true
@@ -12,7 +15,7 @@ func _ready() -> void:
 	last_pos = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var is_moving = global_position.distance_to(last_pos) > 1.0
 	
 	if is_moving:
@@ -23,12 +26,32 @@ func _process(delta: float) -> void:
 		
 	last_pos = global_position
 	
-	if Input.is_action_just_pressed("E"):
+	if Input.is_action_just_pressed("toggle_blanket"):
 		follow_player = !follow_player
-		print("follow_player:", follow_player)
+		print("E pressed")
+		
+		if follow_player:
+			set_moving(true)
+		else:
+			set_moving(false)
+		#print("follow_player:", follow_player)
 		
 	if Input.is_action_pressed("left_click") and not follow_player:
-		global_position = global_position.lerp(get_global_mouse_position(), SPEED * delta)
+		var target = get_global_mouse_position()
+		
+		global_position = global_position.lerp(target, SPEED * delta)
+
+func set_moving(moving: bool):
+	if moving:
+		anim_body.visible = true
+		static_body.process_mode = Node.PROCESS_MODE_DISABLED
+		static_body.collision_layer = 0
+		static_body.collision_mask = 0
+	else:
+		anim_body.visible = true
+		static_body.process_mode = Node.PROCESS_MODE_INHERIT
+		static_body.collision_layer = 1
+		static_body.collision_mask = 1
 
 	
 	
--- scenes/world.gd
+++ scenes/world.gd
@@ -3,11 +3,12 @@ extends Node2D
 @onready var fade_out: AnimationPlayer = $ColorRect/AnimationPlayer
 @onready var timer: Timer = $Timer
 @onready var player: CharacterBody2D = $Player
-@onready var magic_blanket: StaticBody2D = $magic_blanket
+@onready var magic_blanket: Node2D = $magic_blanket
 @onready var blanket_follow: Label = $Player/Camera2D/blanket_follow
 
 const FOLLOW_SPEED = 4.0
 const DISTANCE = 15.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,14 +19,18 @@ func _ready() -> void:
 func _physics_process(delta):
 	var direction = player.velocity.normalized()
 	
+	#print(magic_blanket.follow_player)
+	
 	if magic_blanket.follow_player:
 		blanket_follow.text = "magic blanket follow: on"
 	else:
 		blanket_follow.text = "magic blanket follow: off"
 	
-	if direction != Vector2.ZERO and magic_blanket.follow_player:
-		var target = player.global_position - direction * DISTANCE
-		magic_blanket.global_position = magic_blanket.global_position.lerp(target, delta * FOLLOW_SPEED)
+	if magic_blanket.follow_player:
+		if direction != Vector2.ZERO:
+			var target = player.global_position - direction * DISTANCE
+			magic_blanket.global_position = magic_blanket.global_position.lerp(target, delta * FOLLOW_SPEED)
+		
 
 
 func _on_timer_timeout() -> void:
