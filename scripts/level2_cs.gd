432a70b273895b2ae883733e00d02c3b28449fff
sekitano <mauziofhope07@gmail.com>
2026-07-26 00:06:55 +0100

    level 1-2 transition complete

--- scripts/level2_cs.gd
+++ scripts/level2_cs.gd
@@ -2,29 +2,45 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var magic_blanket: Node2D = $magic_blanket
-@onready var blanket_follow: Label = $blanket_follow
-
-
+@onready var blanket_follow: Label = $Path2D/PathFollow2D/Camera2D/blanket_follow
 
 #@onready var blanket_follow: Label = $Player/Camera2D/blanket_follow
+@onready var camera: Camera2D = $Path2D/PathFollow2D/Camera2D
+@onready var path_follower: PathFollow2D = $Path2D/PathFollow2D
 
 const FOLLOW_SPEED = 4.0
 const DISTANCE = 15.0
 
+var default_zoom := Vector2(0.6, 0.6)
+var zoomed_out := Vector2(1.15, 1.15)
+#var zoom_in_speed_sec := 0.01
+var zoom_out_speed_sec := 1
+
+var is_path_following = false
 
 #var tween = get_tree().create_tween()
 #tween.tween_property(camera, "zoom", zoomed_out, zoom_out_speed_sec)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	player.set_physics_process(false)
+	player.animated_sprite.play("idle")
+	is_path_following = true
 
 func _process(delta):
 	pass
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
+	if is_path_following:
+		path_follower.progress_ratio += 0.0051
+		var tween = get_tree().create_tween()
+		tween.tween_property(camera, "zoom", zoomed_out, zoom_out_speed_sec)
+		if path_follower.progress_ratio >= 1:
+			is_path_following = false
+			get_tree().change_scene_to_file('res://scenes/level2.tscn')
+			#player.set_physics_process(true)
+			
 	var direction = player.velocity.normalized()
 	
 	#print(magic_blanket.follow_player)
