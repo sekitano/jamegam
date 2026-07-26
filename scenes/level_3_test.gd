89fefde1ac13473b1801559ea2a5e330c3bcb213
sekitano <mauziofhope07@gmail.com>
2026-07-26 04:04:02 +0100

    level 3 added, collisions not working or smth

--- scenes/level_3_test.gd
+++ scenes/level_3_test.gd
@@ -5,8 +5,9 @@ extends Node2D
 @onready var blanket_follow: Label = $blanket_follow
 @onready var path_2d: Path2D = $Path2D
 @onready var path_follower: PathFollow2D = $Path2D/PathFollow2D
-@onready var camera: Camera2D = $Path2D/PathFollow2D/Camera2D
 @onready var player_cam: Camera2D = $Player/Camera2D
+@onready var camera: Camera2D = $Path2D/PathFollow2D/Camera2D
+
 
 
 
@@ -16,7 +17,6 @@ var default_zoom := Vector2(3.19, 3.19)
 var zoomed_out := Vector2(1.15, 1.15)
 var zoom_in_speed_sec := 0.3
 var zoom_out_speed_sec := 1
-@onready var camera_2d_2: Camera2D = $Path2D/PathFollow2D/Camera2D2
 
 const FOLLOW_SPEED = 4.0
 const DISTANCE = 15.0
@@ -27,19 +27,22 @@ var is_path_following = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player.set_physics_process(false)
+	player.animated_sprite.play("idle")
 	camera.make_current()
+	is_path_following = true
+	
 func _process(delta):
 	pass
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if is_path_following:
-		path_follower.progress_ratio += 0.02
+		path_follower.progress_ratio += 0.0051
 		var tween = get_tree().create_tween()
-		tween.tween_property(camera, "zoom", default_zoom, zoom_in_speed_sec)
+		tween.tween_property(camera, "zoom", zoomed_out, zoom_out_speed_sec)
 		
 		if path_follower.progress_ratio >= 1:
-			player_cam.make_current()
 			player.set_physics_process(true)
 			is_path_following = false
 		
@@ -58,14 +61,6 @@ func _physics_process(delta):
 			magic_blanket.global_position = magic_blanket.global_position.lerp(target, delta * FOLLOW_SPEED)
 		
 
+func _on_kill_bricks_body_entered(body: Node2D) -> void:
 
-func _on_transition_body_entered(body: Node2D) -> void:
-	is_path_following = true
-	player.set_physics_process(false)
-
-
-func _on_transition_to_lvl_3_body_entered(body: Node2D) -> void:
-	call_deferred("load_lvl3")
-
-func load_lvl3():
-	get_tree().change_scene_to_file('res://scenes/level3.tscn')
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
