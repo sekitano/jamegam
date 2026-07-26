c96e71576d2092f8ff91ae362ce5f3bc24209d22
sekitano <mauziofhope07@gmail.com>
2026-07-26 23:00:40 +0100

    last one

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var timer: Timer = $Timer
 @onready var label_2: Label = $Label2
+@onready var timer_2: Timer = $Timer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,8 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	label_2.show()
+	timer_2.start()
+
+
+func _on_timer_2_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
